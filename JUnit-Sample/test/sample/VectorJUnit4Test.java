babbbb35ed307782aae2f0ffee41daa72d115f24
IvannaR <ivana.ristova95@yahoo.com>
2016-05-10 00:09:29 +0200

    add junit

---
+++ JUnit-Sample/test/sample/VectorJUnit4Test.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Andrej_
+ */
+public class VectorJUnit4Test {
+    
+    public VectorJUnit4Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of equal method, of class Vectors.
+     */
+    @Test
+    public void equalsCheck() {
+        /*System.out.println("equal");
+        int[] a = null;
+        int[] b = null;
+        boolean expResult = false;
+        boolean result = Vectors.equal(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");*/
+        System.out.println("* VectorsJUnit4Test: equalsCheck()");
+        assertTrue(Vectors.equal(new int[] {}, new int[] {}));
+        assertTrue(Vectors.equal(new int[] {0}, new int[] {0}));
+        assertTrue(Vectors.equal(new int[] {0, 0}, new int[] {0, 0}));
+        assertTrue(Vectors.equal(new int[] {0,0,0}, new int[] {0,0,0}));
+        assertTrue(Vectors.equal(new int[] {5,6,7}, new int[] {5,6,7}));
+        
+        assertFalse(Vectors.equal(new int[] {}, new int[] {0}));
+        assertFalse(Vectors.equal(new int[] {0}, new int[] {0,0}));
+        assertFalse(Vectors.equal(new int[] {0,0}, new int[] {0,0,0})); 
+        assertFalse(Vectors.equal(new int[] {0,0,0}, new int[] {0,0}));
+        assertFalse(Vectors.equal(new int[] {0,0}, new int[] {0}));
+        assertFalse(Vectors.equal(new int[] {0}, new int[] {}));
+        
+        assertFalse(Vectors.equal(new int[] {0,0,0}, new int[] {0,0,1}));
+        assertFalse(Vectors.equal(new int[] {0,0,0}, new int[] {0,1,0}));
+        assertFalse(Vectors.equal(new int[] {0,0,0}, new int[] {1,0,0}));
+        assertFalse(Vectors.equal(new int[] {0,0,1}, new int[] {0,0,3}));
+        
+    
+    }
+
+    /**
+     * Test of scalarMultiplication method, of class Vectors.
+     */
+    @Test
+    public void ScalarMultiplicationCheck() {
+        System.out.println("*VectorJUnit4Test: ScalarMultiplicationCheck");
+        /*int[] a = null;
+        int[] b = null;
+        int expResult = 0;
+        int result = Vectors.scalarMultiplication(a, b);*/
+        assertEquals(0, Vectors.scalarMultiplication(new int[] {0, 0}, new int[] {0, 0}));
+        assertEquals(39, Vectors.scalarMultiplication(new int[] {3, 4}, new int[] {5, 6}));
+        assertEquals(-39, Vectors.scalarMultiplication(new int[] {-3, 4}, new int[] {5, -6}));
+        assertEquals(0, Vectors.scalarMultiplication(new int[] {5, 9}, new int[] {-9, 5}));
+        assertEquals(100, Vectors.scalarMultiplication(new int[] {6, 8}, new int[] {6, 8}));
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
